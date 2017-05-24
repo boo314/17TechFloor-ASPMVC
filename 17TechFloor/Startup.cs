67346f36ec8800dda25e47a2dca13846aef5732f
Piotr Cichocki <cichockiweb@gmail.com>
2017-05-24 21:03:26 +0200

    init

---
+++ 17TechFloor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_17TechFloor.Startup))]
+namespace _17TechFloor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
